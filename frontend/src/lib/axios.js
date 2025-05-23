f09112d67490ce25e10b775af70c1035b012e5ca
sindhuja232 <22eg107a28@anurag.edu.in>
2025-05-23 16:26:27 +0530

    PROJECT completed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,8 @@
-
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api",
   withCredentials: true,
 });
 
-export default instance;
+export default axiosInstance;
